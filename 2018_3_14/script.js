a4cab6f6d14d2a53fb32ba35d9b69fc0bfadc4e8
Myka Martinez <myka@mykaintech.com>
2018-03-21 13:18:49 -0500

    Added file structure for 2018_3_21

--- 2018_3_14/script.js
+++ 2018_3_14/script.js
@@ -2,10 +2,18 @@ console.log("Connected")
 
 let inputArray = [1, 2, 3, 4, 5]
 
-let arrayHash = {}
+let product = 1
 
-for (let i = 0; i < inputArray.length; i++) {
-	arrayHash.push{i: inputArray[i]}
+for (let i = 0;  i < inputArray.length; i++){
+	product *= inputArray[i]
 }
 
-console.log(arrayHash)
+console.log(product)
+
+// let arrayHash = {}
+
+// for (let i = 0; i < inputArray.length; i++) {
+// 	arrayHash[i] = inputArray[i]
+// }
+
+// const outputArray = inputArray.map(i => )
