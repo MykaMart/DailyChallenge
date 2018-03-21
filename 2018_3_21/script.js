3824f9bac223cb0e0e1d547835f7df39b925c1af
Myka Martinez <myka@mykaintech.com>
2018-03-21 15:28:56 -0500

    HTML interface added, Javacript updated for interface and logging cleanup

--- 2018_3_21/script.js
+++ 2018_3_21/script.js
@@ -4,34 +4,29 @@
 // For example, [2, 4, 6, 8] should return 12, since we pick 4 and 8. [5, 1, 1, 5] should return
 // 10, since we pick 5 and 5.
 
-const inputArray  = [5, 1, 1, 5]
+let $inputArray;
+let highest = 0;
 
-let highest = 0
-for(let i = 0; i < inputArray.length; i++){
+$("#arrayForm").submit(function (e) {
+	e.preventDefault();
+	$inputArray = $("#arrayInput").val();
+	$inputArray= $inputArray.split(', ').map(Number);
 
-	console.log("Testing for " + i)
-	console.log("-------------------------")
-	for(let t = 0; t < $inputArray.length; t++){
-		console.log("Index " + i + " vs Index " + t)
-		 if((t !== i - 1 && t !== i + 1) && (t < i || t > i)){
+	for(let i = 0; i < $inputArray.length; i++){
 
-		 	const test = inputArray[i]+inputArray[t]
-		 	console.log(inputArray[i] + " + " + inputArray[t] + " = " + test)
-		 	if(test > highest) {
-		 		console.log(test + " is higher than current highest sum " + highest)
-		 		highest = test
-		 		console.log("Current highest sum: " + highest)
-		 		console.log("--------------------------------------------------")
-		 	}else {
-		 		console.log(test + " is not higher than current highest sum " + highest)
-		 		console.log("Current highest sum: " + highest)
-		 		console.log("--------------------------------------------------")
-		 	}
-		 }else{
-		 	console.log("Indexes match or are adjacent, no test")
-		 	console.log("--------------------------------------------------")
-		 }
+		for(let t = 0; t < $inputArray.length; t++){
+
+			 if( (t !== i - 1 && t !== i + 1) && (t < i || t > i) ){
+
+			 	const test = $inputArray[i]+$inputArray[t];
+			 	
+			 	if(test > highest) {
+			 		highest = test;
+			 	}
+			 }
+		}
 	}
-}
 
-console.log("Highest sum is " + highest)
+	$("#result").text("Highest sum of non-adjacent numbers is " + highest);
+	highest = 0;
+})
